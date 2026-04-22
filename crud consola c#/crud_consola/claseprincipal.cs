7b952769c5664870f8226686df1d3006e53539e0
angel6a2118netizen <ama6a2118@gmail.com>
2026-04-21 20:13:07 -0400

    confirmado

---
+++ crud consola c#/crud_consola/claseprincipal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud_consola
+{
+    internal class claseprincipal
+    {
+        static void Main(string[] args)
+        {
+                personaservice service = new personaservice();
+
+            while (true) {
+
+
+                Console.WriteLine("\n1. Crear");
+                Console.WriteLine("2. Ver");
+                Console.WriteLine("3. Actualizar");
+                Console.WriteLine("4. Eliminar");
+                Console.WriteLine("5. Salir");
+
+                string op = Console.ReadLine();
+
+                switch (op) {
+
+                    case "1": service.crearpersona(); break;
+                    case "2": service.mostrarpersonas(); break;
+                    case "3": service.actualizarpersonas(); break;
+                    case "4": service.eliminarpersonas(); break;
+                    case "5": return;
+
+                }
+            }
+
+        }
+    }
+}
---
+++ crud consola c#/crud_consola/personaservice.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace crud_consola
+{
+    public class personaservice
+    {
+        private List<persona> listapersona = new List<persona>();
+
+        public void crearpersona()
+        {
+            Console.WriteLine("cual es tu nombre? ");
+            string nombre = Console.ReadLine();
+
+            persona p = new persona();
+            {
+                Id = listapersona.Count + 1,
+                Nombre = nombre
+
+            };
+
+            listapersona.Add(p);
+            Console.WriteLine("persona agregada.");
+        }
+
+
+        public void mostrarpersonas()
+        {
+            foreach (var p in listapersona)
+            {
+                Console.WriteLine("Id: {p.Id} Nombre: {p.Nombre}");
+
+            }
+        }
+
+        public void actualizarpersonas() {
+            Console.WriteLine("cual es el Id?");
+            int id = int.Parse(Console.ReadLine());
+
+            var persona = listapersona.FirstOrDefault(p => p.Id == id);
+
+            if (persona != null)
+            {
+                Console.WriteLine("nuevo nombre:");
+                persona.Nombre = Console.ReadLine();
+            }
+
+        }
+
+
+        public void eliminarpersonas()
+        {
+            Console.WriteLine("cual es el Id?");
+            int id = int.Parse(Console.ReadLine());
+
+            var persona = listapersona.FirstOrDefault(p => p.Id == id);
+
+            if (persona != null)
+            {
+                listapersona.Remove(persona);
+            }
+        }
+    }
+}
+
